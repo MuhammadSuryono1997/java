ef03277e60c5e2f6675ec4c847e28724dbd7f36f
muhammadsuryono <msuryono0@gmail.com>
2020-07-27 22:39:21 +0900

    Update Task

---
+++ Task/src/com/multidinamika/javabasic/NumberTen.java
@@ -0,0 +1,17 @@
+package com.multidinamika.javabasic;
+
+import java.util.List;
+
+public class NumberTen {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Irisan
+    {
+        private List<Integer> get(List listFirst,List listSecond)
+        {
+            return listFirst;
+        }
+    }
+}
