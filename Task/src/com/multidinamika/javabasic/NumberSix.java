6f25d840581c13a0d56e58b063971ff395facc34
muhammadsuryono <msuryono0@gmail.com>
2020-07-28 16:26:25 +0900

    Add Update

--- Task/src/com/multidinamika/javabasic/NumberSix.java
+++ Task/src/com/multidinamika/javabasic/NumberSix.java
@@ -2,34 +2,29 @@ package com.multidinamika.javabasic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NumberSix {
     public static void main(String[] args) {
-        GanjilGenap oddeven = new GanjilGenap();
-        List<String> lisOddEven = oddeven.cetak(200);
+        List<Integer> range = IntStream.rangeClosed(1,200)
+                .boxed().collect(Collectors.toList());
 
-        for (String ganjilgenap:lisOddEven)
-        {
-            System.out.println(ganjilgenap+", ");
-        }
+        range.stream().forEach(new GanjilGenap()::check);
     }
 
     public static class GanjilGenap
     {
-        private List<String> cetak(int limit)
+        private boolean check(int i)
         {
-            List<String> listOddEven = new ArrayList<String>();
-            for (int i = 1; i < limit+1 ; i++)
-            {
-                if (i % 100 == 0) listOddEven.add("Kelipatan seratus");
-                else if (i%2 == 0)
-                    if (i % 8 == 0) listOddEven.add("Genap dan Kelipatan delapan");
-                    else listOddEven.add("Genap");
-                else if (i%2 == 1)
-                    if (i % 7 == 0) listOddEven.add("Ganjil dan Kelipatan tujuh");
-                    else listOddEven.add("Ganjil");
-            }
-            return listOddEven;
+            if (i % 100 == 0) System.out.println(i+". Kelipatan seratus");
+            else if (i%2 == 0)
+                if (i % 8 == 0) System.out.println(i+". Genap dan Kelipatan delapan");
+                else System.out.println(i+". Genap");
+            else if (i%2 == 1)
+                if (i % 7 == 0) System.out.println(i+". Ganjil dan Kelipatan tujuh");
+                else System.out.println(i+". Ganjil");
+            return true;
         }
     }
 }
